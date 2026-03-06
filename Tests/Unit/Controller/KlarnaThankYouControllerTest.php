0f02f4f15ea2718a2d25a60b59369b0574392ea5
SebastianGoral-fc <sebastian.goral@fatchip.de>
2026-03-06 12:20:48 +0100

    FC#0131418 - remove dead Klarna Instant Basket feature (#22)

--- Tests/Unit/Controller/KlarnaThankYouControllerTest.php
+++ Tests/Unit/Controller/KlarnaThankYouControllerTest.php
@@ -11,7 +11,6 @@ use OxidEsales\Eshop\Core\Registry;
 use TopConcepts\Klarna\Controller\KlarnaThankYouController;
 use TopConcepts\Klarna\Core\Exception\KlarnaClientException;
 use TopConcepts\Klarna\Core\Exception\KlarnaWrongCredentialsException;
-use TopConcepts\Klarna\Model\KlarnaInstantBasket;
 use TopConcepts\Klarna\Tests\Unit\ModuleUnitTestCase;
 
 /**
@@ -49,8 +48,7 @@ class KlarnaThankYouControllerTest extends ModuleUnitTestCase
         $oBasket->expects($this->once())->method('getProductsCount')->will($this->returnValue(1));
         $oBasket->expects($this->once())->method('getOrderId')->will($this->returnValue(1));
 
-        $controller = $this->getMockBuilder(KlarnaThankYouController::class)->
-        setMethods(['getNewKlarnaInstantBasket'])->getMock();
+        $controller = $this->getMockBuilder(KlarnaThankYouController::class)->getMock();
 
         $this->setProtectedClassProperty($controller, '_oBasket', $oBasket);
 
